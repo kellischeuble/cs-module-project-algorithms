0f0bc926194e2425a74adf2984636b8c9352d10c
Kelli Scheuble <github@scheuble.dev>
2020-07-22 14:32:47 -0700

    Single number mvp (non recursive)

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,21 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
 
-    pass
+    double = True
+    
+    i = 0
 
+    while double == True:
+        if arr[i] != arr[i+1]:
+            double = False
+            odd_out = arr[i]
+        i += 2
+
+    return odd_out
+
+    # TODO: 
+    # Recursive implementation
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
